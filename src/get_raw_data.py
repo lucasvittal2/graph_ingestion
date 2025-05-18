b96050f78c148e56158bd2ba5bb61ea2a915f424
lucasvittal2 <lucasvittal@gmail.com>
2025-05-18 13:55:21 -0300

    feat: implemented getting raw data process

---
+++ src/get_raw_data.py
@@ -0,0 +1,123 @@
+import os
+import random
+import requests
+import threading
+from typing import List
+from concurrent.futures import ThreadPoolExecutor
+import logging
+import json
+
+def save_text(data: str,path: str) -> None:
+    try:
+        with open(path, 'w') as file:
+            file.write(data)
+            logging.info(f"Save file successfully at {path} !")
+    except Exception as e:
+        print(e)
+
+def read_text(path: str)-> str | None:
+    try:
+        with open(path, 'r') as file:
+            content = file.read()
+            logging.info(f"Read file {path}")
+            return content
+    except Exception as e:
+        logging.error(f"Failed to read file path {path}: \n\n{e}\n\n")
+        return None
+
+def save_json(data:str, path:str) ->  None:
+    try:
+        with open(path, 'w') as file:
+            json.dump(data, file)
+            logging.info(f"Saved file {path} successfully !")
+
+    except Exception as e:
+        logging.error(f"Failed to save file path {path}: \n\n{e}\n\n")
+        raise e
+def read_json(path: str) -> dict | None:
+    try:
+        with open(path, 'r') as file:
+            data = json.load(file)
+            return data
+    except Exception as e:
+        logging.error(f"Failed to read file path {path}: \n\n{e}\n\n")
+        raise e
+
+def sample_ids()-> None:
+    SAMPLE_SIZE = 50000
+    PATH = "/home/acer/Downloads/PMC040XXXXX_json_ascii"
+    pubmed_ids = [item.replace(".xml", "") for item in os.listdir(PATH)]
+    sample = random.choices(pubmed_ids, k=SAMPLE_SIZE)
+    save_text("|".join(sample), "/home/acer/projects/graph_ingestion/data/pubmed_ids_sample.txt")
+
+
+def fetch_single_id(id, raw_data, lock):
+    try:
+        url = f"https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_json/{id}/unicode"
+        response = requests.get(url)
+        body_response = response.json()
+
+        # Use a lock when writing to the shared dictionary
+        with lock:
+            raw_data[id] = body_response[0]
+            #logging.info(f"Got raw data for ID: {id}")
+
+        return True
+    except Exception as e:
+        logging.error(f"Failed to get raw data for ID {id}: {e}")
+        return False
+
+
+def get_raw_data_threaded(ids: List[str], max_workers=10, continue_process=False):
+    raw_data = {}
+    lock = threading.Lock()  # Create a lock for thread-safe dictionary updates
+    RAW_DATA_PATH = '/home/acer/projects/graph_ingestion/data/raw/pubmed_articles.json'
+    ids_to_process = ids.copy()
+
+    #implement incremental extractions
+    if continue_process:
+        all_ids_set = set(ids_to_process)
+        content_processed = read_json(RAW_DATA_PATH)
+        ids_processed = set(content_processed.keys())
+        ids_to_process = list(all_ids_set.difference(ids_processed))
+
+    total_ids = len(ids_to_process)
+    print(total_ids)
+    successful_fetches = 0
+
+    try:
+        # Use a ThreadPoolExecutor to manage the thread pool
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            # Submit tasks for each ID
+            futures = [executor.submit(fetch_single_id, id, raw_data, lock) for id in ids_to_process]
+
+            # Wait for all futures to complete
+            for future in futures:
+                if future.result():
+                    successful_fetches += 1
+
+        logging.info(
+            f"Extraction of raw data completed. Successfully fetched {successful_fetches}/{total_ids} records.")
+        save_json(raw_data, RAW_DATA_PATH)
+        return raw_data
+
+    except Exception as e:
+        logging.error(f"Failed in thread execution: \n\n{e}\n\n")
+        # Save whatever data was collected before the error
+        if raw_data:
+            save_json(raw_data, RAW_DATA_PATH)
+        return raw_data
+
+def setup_logs()->None:
+    format = (
+        f"[INGESTION-PUBMED-DATA] - [%(asctime)s] - [%(levelname)s] - %(message)s"
+    )
+    logging.basicConfig(format=format, level=logging.INFO)
+
+if __name__=="__main__":
+    setup_logs()
+    #sample  = sample_ids()
+    pubmed_ids = read_text("/home/acer/projects/graph_ingestion/data/pubmed_ids_sample.txt").split("|")
+    get_raw_data_threaded(pubmed_ids, max_workers=20, continue_process=True)
+
+
