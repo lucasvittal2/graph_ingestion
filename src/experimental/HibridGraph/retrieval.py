35814167bc646dd3cc30dd994293aa986780a846
lucasvittal2 <lucasvittal@gmail.com>
2025-06-04 18:25:00 -0300

    refactory: segmented part that communicates with pubmed mesh term api from VectorKGRetriever in order to each class having single responsibility and having a specific function.

--- src/experimental/HibridGraph/retrieval.py
+++ src/experimental/HibridGraph/retrieval.py
@@ -1,38 +1,64 @@
 from utils import *
+from constants import PUBMED_MESH_API_ENDPOINT
 from SPARQLWrapper import SPARQLWrapper, JSON
 from langchain_core.embeddings import Embeddings
 from rdflib import Graph
 from typing import List
 
-class VectorKGRetriever:
-    def __init__(self, embeddings: Embeddings, logger: Logger) -> None:
+class PubmedMeshTermAPI:
 
-        self.logger = logger
-        self.article_vector_db = PostgresDBVector(collection_name="articles", logger=logger, embeddings=embeddings)
-        self.mesh_terms_vector_db = PostgresDBVector(collection_name="mesh_terms", logger=logger, embeddings=embeddings)
+    def __init__(self, logger: Logger) -> None:
+        self._logger = logger
+        self._ALTERNATIVE_TERMS_BASE_QUERY_PATH = "src/experimental/HibridGraph/queries/query_alternative_terms.rq"
+        self._NARROWER_TERMS_BASE_QUERY_PATH = "src/experimental/HibridGraph/queries/query_narrower_terms.rq"
 
-
-    def __get_alternative_terms(self, term: str) -> List[str]:
+    def __get_narrower_concepts_for_term(self, term: str) -> List[str]:
         term = sanitize_term(term)  # Sanitize input term
-        sparql = SPARQLWrapper("https://id.nlm.nih.gov/mesh/sparql")
-        query = f"""
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX meshv: <http://id.nlm.nih.gov/mesh/vocab#>
-        PREFIX mesh: <http://id.nlm.nih.gov/mesh/>
-    
-        SELECT ?subject ?p ?pLabel ?o ?oLabel
-        FROM <http://id.nlm.nih.gov/mesh>
-        WHERE {{
-            ?subject rdfs:label "{term}"@en .
-            ?subject ?p ?o .
-            FILTER(CONTAINS(STR(?p), "concept"))
-            OPTIONAL {{ ?p rdfs:label ?pLabel . }}
-            OPTIONAL {{ ?o rdfs:label ?oLabel . }}
-        }}
-        """
+        sparql = SPARQLWrapper(PUBMED_MESH_API_ENDPOINT)
+        query = read_text(self._NARROWER_TERMS_BASE_QUERY_PATH, self._logger).format(term=term)
         try:
-            self.logger.info("Getting alternative MeSH terms...")
+            self._logger.info("Getting Narrower MeSH terms...")
+            sparql.setQuery(query)
+            sparql.setReturnFormat(JSON)
+            results = sparql.query().convert()
+
+            concepts = set()
+            for result in results["results"]["bindings"]:
+                subject_label = result.get("narrowerConceptLabel", {}).get("value", "No label")
+                concepts.add(sanitize_term(subject_label))  # Sanitize term before adding
+
+            list_concepts = list(concepts)
+            self._logger.info(f"Got  {len(list_concepts)} Narrower MeSH terms.")
+
+            return list_concepts
+
+        except Exception as err:
+            self._logger.error(f"Error fetching narrower concepts for term '{term}': {err}")
+            raise err
+
+    def get_all_narrower_concepts(self, term, depth=2, current_depth=1):
+        term = sanitize_term(term)  # Sanitize input term
+        all_concepts = []
+        try:
+            narrower_concepts = self.__get_narrower_concepts_for_term(term)
+            all_concepts.extend(narrower_concepts)
+
+            if current_depth < depth:
+                for concept in narrower_concepts:
+                    child_concepts = self.get_all_narrower_concepts(concept, depth, current_depth + 1)
+                    all_concepts.extend(child_concepts)
+
+        except Exception as e:
+            self._logger.error(f"Error fetching all narrower concepts for term '{term}': {e}")
+
+        return all_concepts
+
+    def get_alternative_terms(self, term: str) -> List[str]:
+        term = sanitize_term(term)  # Sanitize input term
+        sparql = SPARQLWrapper(PUBMED_MESH_API_ENDPOINT)
+        query = read_text(self._ALTERNATIVE_TERMS_BASE_QUERY_PATH, self._logger).format(term=term)
+        try:
+            self._logger.info("Getting alternative MeSH terms...")
             sparql.setQuery(query)
             sparql.setReturnFormat(JSON)
             results = sparql.query().convert()
@@ -45,102 +71,38 @@ class VectorKGRetriever:
             # Add the sanitized term itself to ensure it's included
             triples.add(sanitize_term(term))
             list_triples = list(triples)
-            self.logger.info(f"Got {len(list_triples)} alternative terms.")
+            self._logger.info(f"Got {len(list_triples)} alternative terms.")
             print("\n")
 
             return list_triples
 
         except Exception as err:
-            self.logger.error(f"Error fetching concept triples for term '{term}': {err}")
+            self._logger.error(f"Error fetching concept triples for term '{term}': {err}")
             raise err
-
-    def __get_narrower_concepts_for_term(self, term: str) -> List[str]:
-        term = sanitize_term(term)  # Sanitize input term
-        sparql = SPARQLWrapper("https://id.nlm.nih.gov/mesh/sparql")
-        query = f"""
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX meshv: <http://id.nlm.nih.gov/mesh/vocab#>
-        PREFIX mesh: <http://id.nlm.nih.gov/mesh/>
     
-        SELECT ?narrowerConcept ?narrowerConceptLabel
-        WHERE {{
-            ?broaderConcept rdfs:label "{term}"@en .
-            ?narrowerConcept meshv:broaderDescriptor ?broaderConcept .
-            ?narrowerConcept rdfs:label ?narrowerConceptLabel .
-        }}
-        """
-        try:
-            self.logger.info("Getting Narrower MeSH terms...")
-            sparql.setQuery(query)
-            sparql.setReturnFormat(JSON)
-            results = sparql.query().convert()
+    
+class VectorKGRetriever:
+    def __init__(self, embeddings: Embeddings, logger: Logger) -> None:
 
-            concepts = set()
-            for result in results["results"]["bindings"]:
-                subject_label = result.get("narrowerConceptLabel", {}).get("value", "No label")
-                concepts.add(sanitize_term(subject_label))  # Sanitize term before adding
-
-            list_concepts = list(concepts)
-            self.logger.info(f"Got  {len(list_concepts)} Narrower MeSH terms.")
-
-            return list_concepts
-
-        except Exception as err:
-            self.logger.error(f"Error fetching narrower concepts for term '{term}': {err}")
-            raise err
-
-
-
-    def __get_all_narrower_concepts(self, term, depth=2, current_depth=1):
-        term = sanitize_term(term)  # Sanitize input term
-        all_concepts = []
-        try:
-            narrower_concepts = self.__get_narrower_concepts_for_term(term)
-            all_concepts.extend(narrower_concepts)
-
-            if current_depth < depth:
-                for concept in narrower_concepts:
-                    child_concepts = self.__get_all_narrower_concepts(concept, depth, current_depth + 1)
-                    all_concepts.extend(child_concepts)
-
-        except Exception as e:
-            self.logger.error(f"Error fetching all narrower concepts for term '{term}': {e}")
-
-        return all_concepts
-
+        self._logger = logger
+        self._pubmed_mesh_api = PubmedMeshTermAPI(logger=self._logger)
+        self._article_vector_db = PostgresDBVector(collection_name="articles", logger=logger, embeddings=embeddings)
+        self._mesh_terms_vector_db = PostgresDBVector(collection_name="mesh_terms", logger=logger, embeddings=embeddings)
+        self._BASE_QUERY_PATH = "src/experimental/HibridGraph/queries/query_graph_knowledge_base.rq"
 
     def __query_rdf(self, local_file_path: str, query: str, mesh_terms: List[str], base_namespace:str="http://example.org/mesh/", top_k:int = 10) -> list:
         if not mesh_terms:
             raise ValueError("The list of MeSH terms is empty or invalid.")
-        self.logger.info("Querying RDF Knowledge graph...\n\n")
-        self.logger.info(f"User's query: {query}")
-        sparql_query = """
-                                PREFIX schema: <http://schema.org/>
-                                PREFIX ex: <http://example.org/>
-    
-                                SELECT ?article ?title ?abstract ?datePublished ?access ?meshTerm
-                                WHERE {{
-                                  ?article a ex:Article ;
-                                           schema:name ?title ;
-                                           schema:description ?abstract ;
-                                           schema:datePublished ?datePublished ;
-                                           ex:access ?access ;
-                                           schema:about ?meshTerm .
-    
-                                  ?meshTerm a ex:MeSHTerm .
-                                  FILTER (?article IN ({article_uris}))
-                                  
-                                  
-                                }}
-                                """
+        self._logger.info("Querying RDF Knowledge graph...\n\n")
+        self._logger.info(f"User's query: {query}")
+        sparql_query = read_text(self._BASE_QUERY_PATH, self._logger)
 
         print("\n\n")
         # Create and parse the RDF graph
         g = Graph()
         g.parse(local_file_path, format="ttl")
 
-        articles_result = self.article_vector_db.query_contexts(query)
+        articles_result = self._article_vector_db.query_contexts(query)
         article_uris = [
             result.metadata['article_URI']
             for result in articles_result
@@ -150,13 +112,13 @@ class VectorKGRetriever:
         spark_query = sparql_query.format(article_uris=article_uris_string)
 
         article_data = {}
-        self.logger.info("Converting MeSH terms to a valid URI and query on them...")
+        self._logger.info("Converting MeSH terms to a valid URI and query on them...")
         for term in mesh_terms:
 
             mesh_term_uri = convert_to_uri(term, base_namespace)
 
             results = g.query(spark_query, initBindings={'meshTerm': mesh_term_uri})
-            self.logger.info(f"Queried on Term '{term}' and URI: '{mesh_term_uri}'")
+            self._logger.info(f"Queried on Term '{term}' and URI: '{mesh_term_uri}'")
 
             for row in results:
                 article_uri = row['article']
@@ -169,38 +131,38 @@ class VectorKGRetriever:
                         'meshTerms': set()
                     }
                 article_data[article_uri]['meshTerms'].add(str(row['meshTerm']))
-            self.logger.info(f"Storaged {len(results)} results.")
+            self._logger.info(f"Storaged {len(results)} results.")
 
-        self.logger.info("ranking results...")
+        self._logger.info("ranking results...")
         ranked_articles = sorted(
             article_data.items(),
             key=lambda item: len(item[1]['meshTerms']),
             reverse=True
         )
         top_ranked_articles = ranked_articles[:top_k]
-        self.logger.info("Ranked result by number of match MeshTerms.")
-        self.logger.info(f"Returned {len(top_ranked_articles)} ranked results.")
+        self._logger.info("Ranked result by number of match MeshTerms.")
+        self._logger.info(f"Returned {len(top_ranked_articles)} ranked results.")
         return top_ranked_articles
 
     def get_contexts(self, query: str, graph_path: str, top_k: int = 10) -> list:
         try:
-            self.logger.info("Retrieving contexts...")
-            mesh_terms_result = self.mesh_terms_vector_db.query_contexts(query)
+            self._logger.info("Retrieving contexts...")
+            mesh_terms_result = self._mesh_terms_vector_db.query_contexts(query)
             retrieved_mesh_terms = [doc.page_content for doc in mesh_terms_result]
 
 
-            alternative_terms = self.__get_alternative_terms(query)
-            narrower_terms = self.__get_all_narrower_concepts(query)
+            alternative_terms = self._pubmed_mesh_api.get_alternative_terms(query)
+            narrower_terms = self._pubmed_mesh_api.get_all_narrower_concepts(query)
             mesh_terms = []
             mesh_terms.extend(alternative_terms)
             mesh_terms.extend(narrower_terms)
             mesh_terms.extend(retrieved_mesh_terms)
             contexts = self.__query_rdf(graph_path, query, mesh_terms, top_k=top_k)
-            self.logger.info(f"Retrieved total top {len(contexts)} contexts successfully !")
+            self._logger.info(f"Retrieved total top {len(contexts)} contexts successfully !")
             return contexts
 
         except Exception as err:
-            self.logger.error(f"Error during retrieving contexts: \n\n{err}\n\n")
+            self._logger.error(f"Error during retrieving contexts: \n\n{err}\n\n")
             raise err
 
 
